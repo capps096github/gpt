75f53c84fadbc0b9b3b91088d60edaac1fa145ba
capps096github <capps096github@gmail.com>
2023-06-24 12:58:17 +0300

    initial

--- ChatApp.java
+++ ChatApp.java
@@ -1,44 +1,117 @@
+// import javax.jms.*;
+// import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+// public class ChatApp {
+//     public static void main(String[] args) {
+//         try {
+//             // Create a connection factory
+//             ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+            
+//             // Create a connection
+//             Connection connection = connectionFactory.createConnection();
+//             connection.start();
+            
+//             // Create a session
+//             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            
+//             // Create the chat group topic
+//             Topic chatTopic = session.createTopic("CommunityChatGroup");
+            
+//             // Create the chairman as the admin
+//             User admin = new User("Chairman", true, session, chatTopic);
+//             admin.joinGroup();
+            
+//             // Create other users
+//             User user1 = new User("John Doe", false, session, chatTopic);
+//             User user2 = new User("Jane Smith", false, session, chatTopic);
+//             user1.joinGroup();
+//             user2.joinGroup();
+            
+//             // Send messages
+//             admin.sendMessage("Welcome to the community chat group!");
+//             user1.sendMessage("Hi everyone, I'm new here.");
+//             user2.sendMessage("Nice to meet you, John!");
+            
+//             // Remove a user from the group
+//             user1.leaveGroup();
+            
+//             // Send a message after user1 has left
+//             user2.sendMessage("Too bad John left. We'll miss him!");
+            
+//             // Close the connections
+//             session.close();
+//             connection.close();
+//         } catch (JMSException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
+
+
+
 import javax.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
-
+import java.util.Scanner;
 
 public class ChatApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException{
         try {
             // Create a connection factory
             ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-            
+
             // Create a connection
             Connection connection = connectionFactory.createConnection();
             connection.start();
-            
+
             // Create a session
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            
+
             // Create the chat group topic
             Topic chatTopic = session.createTopic("CommunityChatGroup");
-            
+
+            // Prompt for the admin's name
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter admin's name: ");
+            String adminName = scanner.nextLine();
+
             // Create the chairman as the admin
-            User admin = new User("Chairman", true, session, chatTopic);
+            User admin = new User(adminName, true, session, chatTopic);
             admin.joinGroup();
-            
+
+            // Prompt for other users' names
+            System.out.print("Enter user1's name: ");
+            String user1Name = scanner.nextLine();
+            System.out.print("Enter user2's name: ");
+            String user2Name = scanner.nextLine();
+
             // Create other users
-            User user1 = new User("John Doe", false, session, chatTopic);
-            User user2 = new User("Jane Smith", false, session, chatTopic);
+            User user1 = new User(user1Name, false, session, chatTopic);
+            User user2 = new User(user2Name, false, session, chatTopic);
             user1.joinGroup();
             user2.joinGroup();
-            
-            // Send messages
-            admin.sendMessage("Welcome to the community chat group!");
-            user1.sendMessage("Hi everyone, I'm new here.");
-            user2.sendMessage("Nice to meet you, John!");
-            
+
+            // Prompt for and send messages
+            System.out.print("Enter welcome message: ");
+            String welcomeMessage = scanner.nextLine();
+            admin.sendMessage(welcomeMessage);
+
+            System.out.print("Enter message from user1: ");
+            String user1Message = scanner.nextLine();
+            user1.sendMessage(user1Message);
+
+            System.out.print("Enter message from user2: ");
+            String user2Message = scanner.nextLine();
+            user2.sendMessage(user2Message);
+
             // Remove a user from the group
             user1.leaveGroup();
-            
-            // Send a message after user1 has left
-            user2.sendMessage("Too bad John left. We'll miss him!");
-            
+
+            // Prompt for and send a message after user1 has left
+            System.out.print("Enter message from user2 after user1 has left: ");
+            String user2MessageAfterLeave = scanner.nextLine();
+            user2.sendMessage(user2MessageAfterLeave);
+
             // Close the connections
             session.close();
             connection.close();
--- User.java
+++ User.java
@@ -1,6 +1,57 @@
+// import javax.jms.*;
+// import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+// class User {
+//     private String name;
+//     private boolean isAdmin;
+//     private Session session;
+//     private Topic chatTopic;
+//     private MessageProducer producer;
+//     private MessageConsumer consumer;
+    
+//     public User(String name, boolean isAdmin, Session session, Topic chatTopic) throws JMSException {
+//         this.name = name;
+//         this.isAdmin = isAdmin;
+//         this.session = session;
+//         this.chatTopic = chatTopic;
+        
+//         // Create the message producer
+//         producer = session.createProducer(chatTopic);
+        
+//         // Create the message consumer
+//         consumer = session.createConsumer(chatTopic);
+//         consumer.setMessageListener(new MessageListener() {
+//             public void onMessage(Message message) {
+//                 try {
+//                     if (message instanceof TextMessage) {
+//                         TextMessage textMessage = (TextMessage) message;
+//                         System.out.println(name + " received a message: " + textMessage.getText());
+//                     }
+//                 } catch (JMSException e) {
+//                     e.printStackTrace();
+//                 }
+//             }
+//         });
+//     }
+    
+//     public void joinGroup() throws JMSException {
+//         System.out.println(name + " joined the chat group.");
+//     }
+    
+//     public void leaveGroup() throws JMSException {
+//         System.out.println(name + " left the chat group.");
+//     }
+    
+//     public void sendMessage(String message) throws JMSException {
+//         TextMessage textMessage = session.createTextMessage();
+//         textMessage.setText(name + " sent a message: " + message);
+//         producer.send(textMessage);
+//     }
+// }
+
 import javax.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
-
 
 class User {
     private String name;
@@ -9,16 +60,16 @@ class User {
     private Topic chatTopic;
     private MessageProducer producer;
     private MessageConsumer consumer;
-    
+
     public User(String name, boolean isAdmin, Session session, Topic chatTopic) throws JMSException {
         this.name = name;
         this.isAdmin = isAdmin;
         this.session = session;
         this.chatTopic = chatTopic;
-        
+
         // Create the message producer
         producer = session.createProducer(chatTopic);
-        
+
         // Create the message consumer
         consumer = session.createConsumer(chatTopic);
         consumer.setMessageListener(new MessageListener() {
@@ -34,18 +85,19 @@ class User {
             }
         });
     }
-    
+
     public void joinGroup() throws JMSException {
         System.out.println(name + " joined the chat group.");
     }
-    
+
     public void leaveGroup() throws JMSException {
         System.out.println(name + " left the chat group.");
     }
-    
+
     public void sendMessage(String message) throws JMSException {
         TextMessage textMessage = session.createTextMessage();
         textMessage.setText(name + " sent a message: " + message);
         producer.send(textMessage);
     }
 }
+
